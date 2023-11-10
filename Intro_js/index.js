d25461da6970e63c12c6ad10067f49d8c385b0a8
Aluno <Aluno@PR01-COMP2-09.CSED.LAB>
2023-11-10 09:25:09 -0300

    att

---
+++ Intro_js/index.js
@@ -0,0 +1,17 @@
+console.log("Arquivo index.js carregado com sucesso!");
+
+//var btnTerceiro = document.getElementsByClassName('btnTerceiro')[0];
+var btnTerceiro = document.querySelector(".btnTerceiro");
+console.log(btnTerceiro);
+
+btnTerceiro.addEventListener("click", () => {
+  var red = Math.floor(Math.random() * 255);
+  var green = Math.floor(Math.random() * 255);
+  var blue = Math.floor(Math.random() * 255);
+
+  var corAleatoria = "rgb(" + red + "," + green + "," + blue + ")";
+
+  console.log(corAleatoria);
+
+  document.body.style.backgroundColor = corAleatoria;
+});
